0b8a0175e597aa433cd620d4705ba1b12179caa3
Bailey <peybailey@FVFZMYE6J1WK.tld>
2022-03-17 14:51:54 -0500

    Second Commit. 3-17-2022.

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ if (ready == "yes") {
     alert("Too bad.")
 }
 
-alert("Alright. So we're both going to draw numbers from 0 to 10. We can draw a maximum of 5 numbers, because 5 is all we should need.") 
-alert("I will always stop drawing numbers once I reach 16, but you can go draw all 5 cards no matter how many points you have. But remember, try to stay under 20!")
+alert("Alright. So we're both going to draw numbers from 0 to 10.") 
+alert("I will always stop drawing numbers once I reach 16, but you can go draw as many numbers as you want. But remember, try to stay under 20!")
 alert("Also, be sure to pay attention to your screen to see what your total is. And type `yes` if you need another number.")
 alert("Ok. Lets go!")
 
@@ -50,21 +50,20 @@ do {
         var myTotal = myNum1 + myNum2
         alert(`Your second number is ${myNum2}.Your total is ${myTotal}.`);
         console.log(myTotal);
-        var nextNumQ = prompt(`. Would you like to pick another number?`);
+        var nextNumQ = prompt(`Would you like to pick another number?`);
     
         do {
             var nextNum = Math.floor(Math.random() * 11)
-            alert(`Your next number is ${nextNum}.`)
             var myTotal = myTotal + nextNum
-            var nextNumQ = prompt(`Your total is now ${myTotal}. Would you like to pick another number?`)
+            alert(`Your next number is ${nextNum}. Your total is now ${myTotal}. `)
+            var nextNumQ = prompt(`Would you like to pick another number?`)
         } while (nextNumQ != "no") 
 
         if (myTotal > 20) {
             alert(`Your score is over 20. I win.`)
         var cPoints = cPoints + 1
-        var myScoreOver = prompt("would you like to play again?");
-        } 
-        if (nextNumQ != "yes") {
+        var myScoreOver = prompt("Would you like to play again?");
+        } if ((nextNumQ != "yes") && (myTotal < 21)) {
             alert(`Ok. Your Final Total is ${myTotal}.`)
         }
         if (((myTotal > cTotal) && (myTotal <= 20) && (cTotal < 20))) {
@@ -84,6 +83,6 @@ do {
 
 } while (((((win != "no") && (lose != "no") &&  (tie != "no") &&  (cScoreOver != "no") &&  (myScoreOver != "no")))))
 
-if (((((win != "yes") &&  (lose != "yes") &&  (tie != "yes") &&  (cScoreOver != "yes") &&  (myScoreOver != "yes"))))) {
+if (((((win == "no") &&  (lose == "no") &&  (tie == "no") &&  (cScoreOver == "no") &&  (myScoreOver == "no"))))) {
     alert(`Ok. Game over. The Final score was ${cPoints} Points for me, and ${myPoints} Points for you.`)
 } 
